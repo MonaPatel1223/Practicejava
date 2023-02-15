637b81431bd8409f89be1d39cb947283c03e8afa
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-15 01:20:46 +0000

    first java

---
+++ JavaTopic.java
@@ -0,0 +1,14 @@
+package java1;
+/* I cahnge the packagename manual to java*/
+
+public class JavaTopic {
+    public static void main(String[] args)
+    {
+        System.out.println("Java Program");
+        System.out.println("what is class");
+        System.out.println("Package");
+        System.out.println("variable");
+        System.out.println("types");
+
+    }
+}
